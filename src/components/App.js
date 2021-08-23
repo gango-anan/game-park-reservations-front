dac906cf2a3d91d1976280e1ce16c0425bd16043
Galiwango Ananiya <galiwango101@gmail.com>
2021-08-23 21:02:45 +0300

    Remove unused code

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
-/* eslint-disable */
 import React from 'react';
+import Registration from './Registration';
 
 function App() {
   return (
     <div className="App">
       <h1>Game Safari Reservations</h1>
+      <main>
+        <Registration />
+      </main>
     </div>
   );
 }
