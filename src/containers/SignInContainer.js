3541484c42612f1e2f272645dd10260fb5f47bfc
Galiwango Ananiya <galiwango101@gmail.com>
2021-08-25 20:15:54 +0300

    Add SignInContainer for handling user sign in

--- src/containers/SignInContainer.js
+++ src/containers/SignInContainer.js
@@ -1,42 +1,47 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import SignInForm from '../components/SignInForm';
+import { signInUser } from '../actions/usersActions';
 
-const SignInContainer = () => (
-  <>
-    <div>
-      <h3>Sign In</h3>
-    </div>
-    <form>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          id="email"
-          type="email"
-          name="email"
-          placeholder="email@example.com"
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input
-          id="password"
-          type="password"
-          name="password"
-          placeholder="Passowrd"
-          required
-        />
-      </div>
-      <div>
-        <label>
-          Show passord
-          <input type="checkbox" name="showPassword" />
-        </label>
-      </div>
-      <div>
-        <button type="submit">Sign In</button>
-      </div>
-    </form>
-  </>
-);
+const SignInContainer = ({ logInUser }) => {
+  const userEmail = useRef(null);
+  const userPassWord = useRef(null);
 
-export default SignInContainer;
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const data = {
+      user: {
+        email: userEmail.current.value,
+        password: userPassWord.current.value,
+      },
+    };
+
+    logInUser(data);
+  };
+  return (
+    <>
+      <div>
+        <h3>Sign In</h3>
+      </div>
+      <SignInForm
+        userEmail={userEmail}
+        userPassWord={userPassWord}
+        handleSubmit={handleSubmit}
+      />
+    </>
+  );
+};
+
+SignInContainer.propTypes = {
+  logInUser: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  logInUser: (logInCredentials) => {
+    dispatch(signInUser(logInCredentials));
+  },
+});
+
+export default connect(null, mapDispatchToProps)(SignInContainer);
