cfe44d4b217b9c931456acb20ee77de4c032b6a4
Galiwango Ananiya <galiwango101@gmail.com>
2021-08-25 12:43:28 +0300

    Update rooReducer with usersReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 import activitiesReducer from './activities';
+import usersReducer from './users';
 
-const rootReducer = combineReducers({ allActivities: activitiesReducer });
+const rootReducer = combineReducers({
+  allActivities: activitiesReducer,
+  userRegistrationData: usersReducer,
+});
 
 export default rootReducer;
