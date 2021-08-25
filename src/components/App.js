dc9650fbaacbfce23d6fff1ce53fbb2b949aab5d
Galiwango Ananiya <galiwango101@gmail.com>
2021-08-25 12:39:19 +0300

    Update App component to render RegistrationsContainer

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Registration from './Registration';
+import RegistrationsContainer from '../containers/RegistrationsContainer';
 
 function App() {
   return (
     <div className="App">
       <h1>Game Safari Reservations</h1>
       <main>
-        <Registration />
+        <RegistrationsContainer />
       </main>
     </div>
   );
