d15b5aabe1a1540a61d2485d79b6322f4bf64b7a
Galiwango Ananiya <galiwango101@gmail.com>
2021-08-25 13:10:15 +0300

    Add RegistrationsContainer for signing up users

---
+++ src/containers/RegistrationsContainer.js
@@ -0,0 +1,69 @@
+import React, { useRef } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import signUpUser from '../actions/usersActions';
+import LoadingUserData from '../components/LoadingUserData';
+import RegistrationForm from '../components/RegistrationForm';
+
+const RegistrationsContainer = (props) => {
+  const { userData, registerUser } = props;
+  const userName = useRef(null);
+  const userEmail = useRef(null);
+  const userPassWord = useRef(null);
+  const userPassWordConfirmation = useRef(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const data = {
+      user: {
+        username: userName.current.value,
+        email: userEmail.current.value,
+        password: userPassWord.current.value,
+        password_confirmation: userPassWordConfirmation.current.value,
+      },
+    };
+
+    registerUser(data);
+  };
+
+  if (userData.waitingForRegData) {
+    return (
+      <div>
+        <LoadingUserData />
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <RegistrationForm
+        userName={userName}
+        userEmail={userEmail}
+        userPassWord={userPassWord}
+        userPassWordConfirmation={userPassWordConfirmation}
+        handleSubmit={handleSubmit}
+      />
+    </div>
+  );
+};
+
+RegistrationsContainer.propTypes = {
+  userData: PropTypes.instanceOf(Object).isRequired,
+  registerUser: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  userData: state.userRegistrationData,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  registerUser: (newUserData) => {
+    dispatch(signUpUser(newUserData));
+  },
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(RegistrationsContainer);
