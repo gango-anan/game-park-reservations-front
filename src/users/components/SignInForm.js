7b1e41d065203b2b30db400c43896d10806a21e3
Galiwango Ananiya <galiwango101@gmail.com>
2021-08-31 12:51:01 +0300

    Update sign in form to display errors

--- src/users/components/SignInForm.js
+++ src/users/components/SignInForm.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import './AuthenticationForm.css';
 
-const SignInForm = ({ userEmail, userPassWord, handleSubmit }) => {
+const SignInForm = ({
+  userEmail, userPassWord, handleSubmit, error,
+}) => {
   useEffect(() => {
     userEmail.current.focus();
   }, []);
@@ -11,6 +13,9 @@ const SignInForm = ({ userEmail, userPassWord, handleSubmit }) => {
   return (
     <div className="auth-form-wrapper">
       <div className="form-container">
+        <div>
+          <p className="text-danger text-center">{error}</p>
+        </div>
         <form onSubmit={handleSubmit}>
           <div className="form-title">
             <h2>Sign in</h2>
@@ -76,6 +81,7 @@ SignInForm.propTypes = {
   userEmail: PropTypes.instanceOf(Object).isRequired,
   userPassWord: PropTypes.instanceOf(Object).isRequired,
   handleSubmit: PropTypes.func.isRequired,
+  error: PropTypes.string.isRequired,
 };
 
 export default SignInForm;
