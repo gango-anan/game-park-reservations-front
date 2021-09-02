0cb94f6b4e8beb08f19a2a45727512d297bf7ce8
Galiwango Ananiya <galiwango101@gmail.com>
2021-09-02 11:27:29 +0300

    Update the Navigation bar to show only the required links

--- src/shared/components/Navigation/NavigationBar.js
+++ src/shared/components/Navigation/NavigationBar.js
@@ -1,53 +1,48 @@
 /* eslint-disable */
-
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Link, NavLink } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import { Container, Nav } from 'react-bootstrap';
 import logo from '../../../assets/images/logo.jpg';
 import './Navigation.style.css';
 
-const NavigationBar = ({ username }) => {
-  if (username) {
+const NavigationBar = ({ userName }) => {
+  const renderLinks = (username) => {
+    if (username) {
+      return (
+        <>
+          <Nav className="align-links">
+            <Nav.Link as={NavLink} to="/" className="nav-links">
+              Log out
+            </Nav.Link>
+          </Nav>
+        </>
+      );
+    }
     return (
       <>
-        <Navbar collapseOnSelect expand="lg">
-          <Container fluid>
-            <Navbar.Brand href="/" style={{ maxWidth: '15vw' }}>
-              <img
-                src={logo}
-                className="d-inline-block align-top"
-                alt="Game Safaris logo"
-              />
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link to="/activities" className="nav-links">
-                  Activities
-                </Nav.Link>
-                <Nav.Link href="/reservations" className="nav-links">
-                  Reservations
-                </Nav.Link>
-              </Nav>
-              <Nav>
-                <Nav.Link href="/user" className="nav-links">
-                  {username}
-                </Nav.Link>
-                <Nav.Link href="/" className="nav-links">
-                  Log out
-                </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
+        <Nav className="align-links">
+          <Nav.Link as={NavLink} to="/activities" className="nav-links">
+            Activities
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/signin" className="nav-links">
+            Sign in
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/signup" className="nav-links">
+            Sign up
+          </Nav.Link>
+        </Nav>
       </>
     );
-  }
+  };
+
   return (
     <>
       <Navbar collapseOnSelect expand="lg">
         <Container fluid>
-          <Navbar.Brand href="/" style={{ maxWidth: '15vw' }}>
+          <Navbar.Brand as={Link} to="/" style={{ maxWidth: '12vw' }}>
             <img
               src={logo}
               className="d-inline-block align-top"
@@ -56,14 +51,7 @@ const NavigationBar = ({ username }) => {
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="align-links">
-              <Nav.Link href="/signin" className="nav-links">
-                Sign in
-              </Nav.Link>
-              <Nav.Link href="/signup" className="nav-links">
-                Sign up
-              </Nav.Link>
-            </Nav>
+            {renderLinks(userName)}
           </Navbar.Collapse>
         </Container>
       </Navbar>
@@ -71,4 +59,11 @@ const NavigationBar = ({ username }) => {
   );
 };
 
-export default NavigationBar;
+NavigationBar.propTypes = {
+  userName: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  userName: state.userCredentials.username,
+});
+export default connect(mapStateToProps)(NavigationBar);
