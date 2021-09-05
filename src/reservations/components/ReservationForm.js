3502ca10654433a8edb2ab3a2398d511e3edaea4
Galiwango Ananiya <galiwango101@gmail.com>
2021-09-05 21:00:28 +0300

    Update Reservations form and add logic to make reservations

--- src/reservations/components/ReservationForm.js
+++ src/reservations/components/ReservationForm.js
@@ -1,61 +1,94 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, Form, Button } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 import './ReservationForm.css';
 
-const ReservationForm = () => (
-  <div>
-    <Container>
-      <Form>
-        <Form.Group className="mb-3" controlId="userName">
-          <Form.Label>Username</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter username"
-            defaultValue="gango"
-            readOnly
-          />
-        </Form.Group>
+const ReservationForm = ({
+  reservationDate,
+  handleSubmit,
+  title,
+  park,
+  username,
+  email,
+  newReservationErrors,
+}) => {
+  useEffect(() => {
+    reservationDate.current.focus();
+  }, []);
 
-        <Form.Group className="mb-3" controlId="email">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="email@galisoft.com"
-            defaultValue="gango@galisoft.com"
-            readOnly
-          />
-        </Form.Group>
+  return (
+    <div>
+      <Container>
+        <Form onSubmit={handleSubmit}>
+          <div>
+            <p className="text-danger">{newReservationErrors}</p>
+          </div>
+          <Form.Group className="mb-3" controlId="userName">
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter username"
+              defaultValue={username}
+              readOnly
+            />
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="activity">
-          <Form.Label>Activity</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter activity"
-            defaultValue="Gorilla Trekking"
-            readOnly
-          />
-        </Form.Group>
+          <Form.Group className="mb-3" controlId="email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="email@galisoft.com"
+              defaultValue={email}
+              readOnly
+            />
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="park">
-          <Form.Label>Park</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter park"
-            defaultValue="Bwindi NP"
-            readOnly
-          />
-        </Form.Group>
+          <Form.Group className="mb-3" controlId="activity">
+            <Form.Label>Activity</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter activity"
+              defaultValue={title}
+              readOnly
+            />
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="reservationDate">
-          <Form.Label>Date</Form.Label>
-          <Form.Control type="date" placeholder="Select date" required />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-    </Container>
-  </div>
-);
+          <Form.Group className="mb-3" controlId="park">
+            <Form.Label>Park</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter park"
+              defaultValue={park}
+              readOnly
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="reservationDate">
+            <Form.Label>Date</Form.Label>
+            <Form.Control
+              type="date"
+              placeholder="Select date"
+              ref={reservationDate}
+              required
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Container>
+    </div>
+  );
+};
+
+ReservationForm.propTypes = {
+  reservationDate: PropTypes.instanceOf(Object).isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+  park: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  newReservationErrors: PropTypes.string.isRequired,
+};
 
 export default ReservationForm;
--- src/reservations/containers/UserReservationsList.js
+++ src/reservations/containers/UserReservationsList.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import Reservation from '../components/Reservation';
-import fetchReservations from '../../actions/reservationsActions';
+import { fetchReservations } from '../../actions/reservationsActions';
 import './UserReservationsList.css';
 
 const UserReservationsList = () => {
