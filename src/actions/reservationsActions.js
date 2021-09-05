1c83b641bb321a10f7df36f8bb7e224b6ecbe5be
Galiwango Ananiya <galiwango101@gmail.com>
2021-09-05 20:45:52 +0300

    Add action for making reservations

--- src/actions/reservationsActions.js
+++ src/actions/reservationsActions.js
@@ -1,17 +1,22 @@
 import axios from 'axios';
 import {
   FETCH_RESERVATIONS,
+  FETCH_RESERVATIONS_SUCCESSFUL,
   FETCH_RESERVATIONS_FAILED,
+  MAKE_RESERVATION,
+  MAKE_RESERVATION_SUCCESSFUL,
+  MAKE_RESERVATION_FAILED,
 } from '../shared/actionTypes';
 
-const fetchReservations = (authToken) => (dispatch) => {
+export const fetchReservations = (authToken) => (dispatch) => {
+  dispatch({ type: FETCH_RESERVATIONS, payload: {} });
   axios
     .get('http://localhost:3000/api/v1/reservations', {
       headers: { Authorization: `${authToken}` },
     })
     .then((response) => {
       dispatch({
-        type: FETCH_RESERVATIONS,
+        type: FETCH_RESERVATIONS_SUCCESSFUL,
         payload: response.data.data,
       });
     })
@@ -23,4 +28,22 @@ const fetchReservations = (authToken) => (dispatch) => {
     });
 };
 
-export default fetchReservations;
+export const makeReservation = (authToken, data) => (dispatch) => {
+  dispatch({ type: MAKE_RESERVATION, payload: {} });
+  axios
+    .post('http://localhost:3000/api/v1/reservations', data, {
+      headers: { Authorization: `${authToken}` },
+    })
+    .then((response) => {
+      dispatch({
+        type: MAKE_RESERVATION_SUCCESSFUL,
+        payload: response.data.data,
+      });
+    })
+    .catch((error) => {
+      dispatch({
+        type: MAKE_RESERVATION_FAILED,
+        payload: error.response.data.join(),
+      });
+    });
+};
